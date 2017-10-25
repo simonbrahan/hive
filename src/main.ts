df944fa8f8514d8a2bbfd0159d4c742600cdd945
simon brahan <tntpiranha@gmail.com>
2017-10-25 19:54:27 +0100

    Moved drawing code to separate module

--- src/main.ts
+++ src/main.ts
@@ -3,53 +3,7 @@
  */
 
 import Layout from './Layout';
-
-var HEX_ANGLES = [
-    radians(0),
-    radians(60),
-    radians(120),
-    radians(180),
-    radians(240),
-    radians(300)
-];
-
-function radians(degrees) {
-    return degrees * Math.PI / 180;
-}
-
-function hexCornerOffset(corner_num, radius) {
-    return {
-        x: radius * Math.cos(HEX_ANGLES[corner_num]),
-        y: radius * Math.sin(HEX_ANGLES[corner_num])
-    };
-}
-
-function addPoints(point, other_point) {
-    return {x: point.x + other_point.x, y: point.y + other_point.y};
-}
-
-function drawPolygon(svg, points) {
-    var pointsAsString = points.reduce(
-        function (builtPath, point) {
-            return builtPath + point.x + ' ' + point.y + ' ';
-        },
-        ''
-    ).trim();
-    var line = document.createElementNS("http://www.w3.org/2000/svg", 'polygon');
-    line.setAttribute('points', pointsAsString);
-    svg.appendChild(line);
-}
-
-function drawHex(svg, centreX, centreY, radius) {
-    var points = [];
-
-    for (var i=0; i<6; i++) {
-        var offset = hexCornerOffset(i, radius);
-        points.push(addPoints({x: centreX, y: centreY}, offset));
-    }
-
-    drawPolygon(svg, points);
-}
+import drawHex from './DrawHex';
 
 window.onload = function() {
     var gameBoard = document.getElementById('game');
